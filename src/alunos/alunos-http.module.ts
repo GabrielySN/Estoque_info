e39c505f98288e68e0e4615f220105570085889a
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:20:53 -0300

    Update alunos-http.module.ts

--- src/alunos/alunos-http.module.ts
+++ src/alunos/alunos-http.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { AlunosController } from "./alunos.controller";
 import { AlunosModule } from "./alunos.module";
 import { AlunosService } from "./alunos.service";
