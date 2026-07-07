d756665f8052b64b7aaed3d2e9340316ddf28dff
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:37:37 -0300

    Update users.controller.ts

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,4 +1,12 @@
-﻿import { Controller, Delete, Get, Param } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
@@ -11,9 +19,58 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Get("perfis")
+  findAllWithProfiles() {
+    return this.usersService.findAllWithProfiles();
+  }
+
+  @Get(":emailEscola/perfil")
+  findProfile(@Param("emailEscola") emailEscola: string) {
+    return this.usersService.findProfile(emailEscola);
+  }
+
   @Get(":emailEscola")
   findOne(@Param("emailEscola") emailEscola: string): Promise<User | null> {
     return this.usersService.findOne(emailEscola);
+  }
+
+  @Post()
+  create(@Body() body): Promise<User> {
+    return this.usersService.create(body);
+  }
+
+  @Post("login")
+  login(@Body() body): Promise<User> {
+    return this.usersService.login(body);
+  }
+
+  @Post("recuperar-senha")
+  requestPasswordRecovery(
+    @Body("emailEscola") emailEscola: string,
+  ): Promise<{ message: string }> {
+    return this.usersService.requestPasswordRecovery(emailEscola);
+  }
+
+  @Post("confirmar-recuperacao")
+  confirmPasswordRecovery(@Body() body): Promise<User> {
+    return this.usersService.confirmPasswordRecovery(
+      body.emailEscola,
+      body.codigo,
+      body.senha,
+    );
+  }
+
+  @Patch(":emailEscola")
+  update(@Param("emailEscola") emailEscola: string, @Body() body): Promise<User> {
+    return this.usersService.update(emailEscola, body);
+  }
+
+  @Patch(":emailEscola/senha")
+  resetPassword(
+    @Param("emailEscola") emailEscola: string,
+    @Body("senha") senha: string,
+  ): Promise<User> {
+    return this.usersService.resetPassword(emailEscola, senha);
   }
 
   @Delete(":emailEscola")
