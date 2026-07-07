3c79f63889fb107c7d006eee483f5dc64a291521
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:23:06 -0300

    Update categorias-http.module.ts

--- src/categorias/categorias-http.module.ts
+++ src/categorias/categorias-http.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { CategoriasController } from "./categorias.controller";
 import { CategoriasModule } from "./categorias.module";
 import { CategoriasService } from "./categorias.service";
