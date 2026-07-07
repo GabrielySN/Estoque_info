61a8cb688517dd51535e8d0be4e25833bca8429e
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:27:40 -0300

    Update emprestimos-http.module.ts

--- src/emprestimos/emprestimos-http.module.ts
+++ src/emprestimos/emprestimos-http.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { EmprestimosController } from "./emprestimos.controller";
 import { EmprestimosModule } from "./emprestimos.modules";
 import { EmprestimosService } from "./emprestimos.service";
