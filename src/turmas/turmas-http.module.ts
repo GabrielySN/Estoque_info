17b13b19e2fe372a36ddaa4cec1803ca1824bea0
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:35:10 -0300

    Update turmas-http.module.ts

--- src/turmas/turmas-http.module.ts
+++ src/turmas/turmas-http.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TurmasController } from "./turmas.controller";
 import { TurmasModule } from "./turmas.module";
 import { TurmasService } from "./turmas.service";
