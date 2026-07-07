cc1b0032e0043e6866ad00e989b6e04cf32ebf13
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:28:34 -0300

    Update emprestimos.modules.ts

--- src/emprestimos/emprestimos.modules.ts
+++ src/emprestimos/emprestimos.modules.ts
@@ -1,10 +1,19 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Item } from "../itens/item.entity";
+import { User } from "../user/user.entity";
 import { EmprestimoAluno } from "./emprestimo-aluno.entity";
 import { EmprestimoProfessor } from "./emprestimo-professor.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmprestimoAluno, EmprestimoProfessor])],
+  imports: [
+    TypeOrmModule.forFeature([
+      EmprestimoAluno,
+      EmprestimoProfessor,
+      Item,
+      User,
+    ]),
+  ],
   exports: [TypeOrmModule],
 })
 export class EmprestimosModule {}
