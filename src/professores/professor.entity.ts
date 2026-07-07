7850124061679ac01c2801476114229c449dadb1
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:32:32 -0300

    Update professor.entity.ts

--- src/professores/professor.entity.ts
+++ src/professores/professor.entity.ts
@@ -1,16 +1,21 @@
-﻿import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity("professor")
 export class Professor {
   @PrimaryColumn({ name: "email_escola", type: "varchar", length: 70 })
   emailEscola: string;
 
-  @Column({ name: "nome_turma", type: "varchar", length: 10 })
-  nomeTurma: string;
+  @Column({
+    name: "tipo_usuario",
+    type: "enum",
+    enum: ["professor"],
+    default: "professor",
+  })
+  tipoUsuario: "professor";
 
-  @Column({ type: "varchar", length: 255 })
-  senha: string;
+  @Column({ name: "nome_turma", type: "varchar", length: 10, nullable: true })
+  nomeTurma: string | null;
 
-  @Column({ type: "varchar", length: 30 })
-  materia: string;
+  @Column({ type: "varchar", length: 30, nullable: true })
+  materia: string | null;
 }
