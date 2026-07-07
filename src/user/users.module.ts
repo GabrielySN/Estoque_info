51c37b8b5fb802ebad7eb0bad9180a0502f92fa2
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:37:56 -0300

    Update users.module.ts

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Aluno } from "../alunos/aluno.entity";
+import { Direcao } from "../direcao/direcao.entity";
+import { Professor } from "../professores/professor.entity";
 import { User } from "./user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Aluno, Direcao, Professor])],
   exports: [TypeOrmModule],
 })
 export class UsersModule {}
