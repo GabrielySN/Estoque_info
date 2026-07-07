735ee74e19fa995c5c789ade731a0c9279ba3743
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:24:58 -0300

    Update direcao-http.module.ts

--- src/direcao/direcao-http.module.ts
+++ src/direcao/direcao-http.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { DirecaoController } from "./direcao.controller";
 import { DirecaoModule } from "./direcao.module";
 import { DirecaoService } from "./direcao.service";
