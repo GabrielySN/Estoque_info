a0259f1d54445c333b930a379825cda7c07f33fb
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:39:21 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AlunosHttpModule } from "./alunos/alunos-http.module";
 import { Aluno } from "./alunos/aluno.entity";
