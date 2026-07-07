e4187440debc76143b6182dc56dd907fad4ddb56
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:21:23 -0300

    Update alunos.controller.ts

--- src/alunos/alunos.controller.ts
+++ src/alunos/alunos.controller.ts
@@ -1,4 +1,4 @@
-﻿import { Controller, Delete, Get, Param } from "@nestjs/common";
+import { Controller, Delete, Get, Param } from "@nestjs/common";
 import { Aluno } from "./aluno.entity";
 import { AlunosService } from "./alunos.service";
 
