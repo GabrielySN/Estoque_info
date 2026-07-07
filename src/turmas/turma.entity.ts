d277d3099221d99d1295a9b2bbab14621477349a
S_N <132583851+GabrielySN@users.noreply.github.com>
2026-07-07 20:34:42 -0300

    Update turma.entity.ts

--- src/turmas/turma.entity.ts
+++ src/turmas/turma.entity.ts
@@ -1,12 +1,16 @@
-﻿import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
-export type TurmaPeriodo = "Manhã" | "Tarde" | "Noite";
+export type TurmaPeriodo = "matutino" | "vespertino" | "noturno";
 
 @Entity("turma")
 export class Turma {
   @PrimaryColumn({ name: "nome_turma", type: "varchar", length: 10 })
   nomeTurma: string;
 
-  @Column({ name: "periodo", type: "enum", enum: ["Manhã", "Tarde", "Noite"] })
+  @Column({
+    name: "periodo",
+    type: "enum",
+    enum: ["matutino", "vespertino", "noturno"],
+  })
   periodo: TurmaPeriodo;
 }
